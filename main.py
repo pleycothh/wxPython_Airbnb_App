b4bb9e71a923749f2704b27cfa8177035ec8d017
BenLi <zhuoheng.li@griffithuni.edu.au>
2021-10-03 19:40:33 +1000

    add check box, print value when click

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ class LeftPanel (wx.Panel): # class need inhertance of wx.Panel
         self.togglebuttonStart = wx.ToggleButton(self, id=-1, label="Start", pos=(10,10))
         self.togglebuttonStart.Bind(wx.EVT_TOGGLEBUTTON, self.OnStartClick)
 
+        labelChannels = wx.StaticText(self, -1, "Input:", pos=(10, 200))
+        self.cb1 = wx.CheckBox(self, -1, label="A0", pos=(10, 220))
+        self.cb2 = wx.CheckBox(self, -1, label="A1", pos=(10, 240))
+
+        self.Bind(wx.EVT_CHECKBOX, self.OnChecked)
+
+    def OnChecked(self, event):
+        cb = event.GetEventObject()
+        print("%s is clicked" % (cb.GetLabel()))
+
     def OnStartClick(self, event):
         value = self.togglebuttonStart.GetValue()
         if value:
