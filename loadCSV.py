4e1a30b877c0b188a3f958f265405814cb6af47b
BenLi <zhuoheng.li@griffithuni.edu.au>
2021-10-04 13:18:59 +1000

    reorganise the file directory

---
+++ loadCSV.py
@@ -0,0 +1,18 @@
+import sqlite3
+import panda as pd
+import numpy as np
+class LoadCSV():
+    def __init__(self):
+        self.load()
+
+    def load(self):
+        print("database")
+
+
+
+
+
+
+
+
+LoadCSV()
