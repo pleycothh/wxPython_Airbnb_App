11cd8bfbe52e17ff501e86cc84cb147b501f4ea3
BenLi <zhuoheng.li@griffithuni.edu.au>
2021-10-04 13:54:22 +1000

    load sample database

--- loadCSV.py
+++ loadCSV.py
@@ -1,10 +1,25 @@
-import sqlite3
+import csv
 
 import numpy as np
 class LoadCSV():
     def __init__(self):
+        self.name = 'src/listings_summary_dec18.csv'
         self.load()
 
-    def load(self):
-        print("database")
 
+    def load(self):
+        with open(self.name, newline='',encoding="utf8") as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            for row in reader:
+                print(row)
+                break
+
+
+
+
+
+
+
+
+LoadCSV()
